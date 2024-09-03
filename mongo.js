a6a6755a906c22018a13318646f8d6c707eb35c5
AKoivisto <antton.v.koivisto@student.jyu.fi>
2024-09-03 15:46:38 +0300

    3.12

--- mongo.js
+++ mongo.js
@@ -19,12 +19,25 @@ const contactSchema = new mongoose.Schema({
 
 const Contact = mongoose.model('Contact', contactSchema)
 
+if (process.argv.length===3) {
+    console.log('phonebook:')
+    Contact.find({}).then(result => {
+        result.forEach(contact => {
+            console.log(contact.name,contact.number)
+        })
+        mongoose.connection.close()
+    })
+  }
+
+else {
 const contact = new Contact({
-    name: 'Arto H',
-    number: '123-123'
+    name: process.argv[3],
+    number: process.argv[4]
 })
 
 contact.save().then(result => {
     console.log('contact saved!')
     mongoose.connection.close()
 })
+
+}
