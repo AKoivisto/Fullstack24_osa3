58ad84b9ab1f5decb6d73dfa0d0609bde9178428
AKoivisto <antton.v.koivisto@student.jyu.fi>
2024-09-04 18:25:41 +0300

    3.22

--- index.js
+++ index.js
@@ -11,18 +11,6 @@ app.post('/api/persons', (request, response, next) => {
   const body = request.body
   morgan_end = request.body
 
-  // if (body.name === undefined) { 
-  //     return response.status(400).json({ 
-  //     error: 'missing name!' 
-  //   })
-  // }
-
-  // if (!body.number === undefined) { 
-  //     return response.status(400).json({ 
-  //     error: 'missing number!' 
-  //   })
-  // }
-
   const contact = new Contact({
     name: body.name,
     number: body.number,
@@ -31,7 +19,7 @@ app.post('/api/persons', (request, response, next) => {
   contact.save().then(savedContact => {
     response.json(savedContact)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 
 })
 
@@ -48,7 +36,7 @@ const errorHandler = (error, request, response, next) => {
 const cors = require('cors')
 
 app.use(cors())
-var morgan_end = ""
+var morgan_end = ''
 morgan.token('data', () => JSON.stringify(morgan_end))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
@@ -56,60 +44,60 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-  
-  app.get('/api/persons', (request, response) => {
-    Contact.find({}).then(contacts => {
-      response.json(contacts)
-    })
+
+app.get('/api/persons', (request, response) => {
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
   })
+})
 
-  app.get('/api/persons/:id', (request, response, next) => {
-    Contact.findById(request.params.id).then(contact => {
-      if (contact) {
-        response.json(contact)
-      } else {
-        response.status(404).end()
-      }
-      })
-      .catch(error => next(error))
-  })
-
-  app.get('/info', (request, response) => {
-    Contact.find({}).then(contacts => {
-      const personCount = contacts.length
-      response.send(`<p>Phonebook has info for ${personCount} people</p> <br>
-        <p>${new Date()}</p>`)
-    })
-
-  })
-
-  app.delete('/api/persons/:id', (request, response, next) => {
-    Contact.findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
-
-  app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    console.log(body)
-    const contact = {
-      name: body.name,
-      number: body.number,
+app.get('/api/persons/:id', (request, response, next) => {
+  Contact.findById(request.params.id).then(contact => {
+    if (contact) {
+      response.json(contact)
+    } else {
+      response.status(404).end()
     }
+  })
+    .catch(error => next(error))
+})
 
-    Contact.findByIdAndUpdate(request.params.id, contact, {new: true})
-      .then(updatedContact => {
-        response.json(updatedContact)
-      })
-      .catch(error => next(error))
+app.get('/info', (request, response) => {
+  Contact.find({}).then(contacts => {
+    const personCount = contacts.length
+    response.send(`<p>Phonebook has info for ${personCount} people</p> <br>
+        <p>${new Date()}</p>`)
   })
 
-  app.use(unknownEndpoint)
-  app.use(errorHandler)
+})
 
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Contact.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  console.log(body)
+  const contact = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+    .then(updatedContact => {
+      response.json(updatedContact)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
